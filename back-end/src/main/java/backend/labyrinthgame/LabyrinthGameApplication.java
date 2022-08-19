1762d238b887ea7f943426f10a1795f8c9667a2f
Lengyel Tamás <lentom71@gmail.com>
2022-08-19 16:18:11 +0200

    Add Spring boot project

---
+++ back-end/src/main/java/backend/labyrinthgame/LabyrinthGameApplication.java
@@ -0,0 +1,14 @@
+package backend.labyrinthgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabyrinthGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabyrinthGameApplication.class, args);
+
+    }
+
+}
---
+++ back-end/src/test/java/backend/labyrinthgame/LabyrinthGameApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.labyrinthgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabyrinthGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
